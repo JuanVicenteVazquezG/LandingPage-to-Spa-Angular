dcbd15ecf7212cad243df5265b59e6b3c1d3fcda
JuanVicenteVazquezG <juanvicentevazquezg@gmail.com>
2020-08-26 21:23:49 +0200

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SliderComponent } from './components/slider/slider.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SliderComponent
+    SliderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
